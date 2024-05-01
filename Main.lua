2a60fad7ed159778588d7cd7e0d3781682dfa2c6
MercyfulSmoked <86445121+MercyfulSmoked@users.noreply.github.com>
2024-05-01 08:08:22 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,5 @@
 if game.GameId == 5361032378 then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/MercyfulSmoked/Xeo/main/SolsRNG.lua'))()
+elseif game.GameId == 5066913846 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/MercyfulSmoked/Xeo/main/Spongebob.lua'))()
 end;
